490ee31833966dbe1d5a00bc0572c2cab352fd61
TWalk007 <walkertb03@gmail.com>
2017-10-30 14:52:47 -0400

    SelectionController
    Starting to move the highlights to a separate controller game object.

---
+++ Assets/Scripts/HighlightController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class HighlightController : MonoBehaviour {
+
+    public Material[] ballMaterials = new Material[2];
+    public Material[] pasteMaterials = new Material[2];
+
+    public Material highlightMaterial;
+    
+    private Material startMaterial;
+
+    private GameObject collidingObject;
+
+    private void Start () {
+        startMaterial = GetComponent<Renderer>().material;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision Triggered! Colliding with " + other.gameObject.name);
+        GetComponent<Renderer>().material = highlightMaterial;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GetComponent<Renderer>().material = startMaterial;
+    }
+}
